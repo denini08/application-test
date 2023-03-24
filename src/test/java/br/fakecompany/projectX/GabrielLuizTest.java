7a2a2ef47bdbd0dc39035e261c8414566b26ea27
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-03-24 00:13:39 -0300

    feat: add new tests

--- src/test/java/br/fakecompany/projectX/GabrielLuizTest.java
+++ src/test/java/br/fakecompany/projectX/GabrielLuizTest.java
@@ -21,8 +21,6 @@ public class GabrielLuizTest {
         assertFalse(result);
         assertTrue(araraIsPalidrome);
     }
-
-
     @Test
     public void testFail(){
         assertTrue(true);
@@ -40,8 +38,15 @@ public class GabrielLuizTest {
      *  }
      */
     @Test(expected = IllegalArgumentException.class)
-    public void testFactorial(){        
+    public void testFactorialException(){
             myUtils.factorial(-1);
+    }
+
+    @Test
+    public void testFactorial(){
+        int result   = myUtils.factorial(5);
+        int expected = 120;
+        assertEquals(expected,result);
     }
 
     @Test
@@ -62,4 +67,40 @@ public class GabrielLuizTest {
         int esperado  = -1;
         assertEquals(esperado,resultado);
     }
+    @Test
+    public void testFindMinimum(){
+        int resultNull = myUtils.findMinimum(null);
+        int expected   = -1;
+        assertEquals(expected,resultNull);
+
+        List<Integer> emptyList = new ArrayList<>();
+        int resultEmpty = myUtils.findMinimum(emptyList);
+        assertEquals(expected,resultEmpty);
+
+        List<Integer> numbersList = Arrays.asList(3,5,20);
+        int numbersListMinValue = 3;
+        int result = myUtils.findMinimum(numbersList);
+        assertEquals(numbersListMinValue,result);
+    }
+
+    @Test
+    public void testIsListInAscendingOrder(){
+        List<Integer> nullList = null;
+        List<Integer> listaCrescente   = Arrays.asList(1,2,3,4,5,6,7,8,9,10,20);
+        List<Integer> listaDescendente = Arrays.asList(10,9,8,7,6,5,4,3,2,1);
+        List<Integer> listaDesordenada = Arrays.asList(12,3,20,40);
+
+            Boolean listaCheia       = myUtils.isListInAscendingOrder(null);
+                assertFalse(listaCheia);
+
+            Boolean alistaCrescente  = myUtils.isListInAscendingOrder(listaCrescente);
+                assertTrue(alistaCrescente);
+
+            Boolean aListaCrescente  = myUtils.isListInAscendingOrder(listaDescendente);
+                assertFalse(aListaCrescente);
+
+            Boolean aListaOrdenada  =  myUtils.isListInAscendingOrder(listaDesordenada);
+                assertFalse(aListaOrdenada);
+    }
+
 }
