7acb07b1e7bd8e0b9fc19bc7444af2c1b7a3161c
Juely2021 <juely.menezes@gmail.com>
2023-03-30 19:44:33 -0300

    exercicio 30/03/2023 testes unitarios Juely

---
+++ src/test/java/br/fakecompany/projectX/JuelyTeste.java
@@ -0,0 +1,134 @@
+package br.fakecompany.projectX;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+
+import org.junit.Ignore;
+
+public class JuelyTeste {
+
+    private MyUtils myUtils = new MyUtils();
+
+    @Test
+    public void testIsPalindrome() {
+        String palindrome = "racecar";
+        assertTrue(myUtils.isPalindrome(palindrome));
+    }
+
+
+    @Test
+    public void testFail(){
+        assertTrue(true);
+    }
+    
+    // @Test
+    // public void testFactorial() {
+    // 	assertEquals(myUtils.factorial(5), 120);
+    // 	assertEquals(0,myUtils.factorial(0));
+    	
+    // }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testFactorial2() {
+    	assertEquals(myUtils.factorial(-2), 0);
+    }
+    
+    // @Test
+    // public void testisPalindrome() {
+    //     assertTrue(myUtils.isPalindrome("natan"));
+    //     assertFalse(myUtils.isPalindrome(null));
+    // }
+    
+    @Test
+    public void testeconverterNumerosRomanos() {
+    	assertEquals(myUtils.converterNumerosRomanos("III"), 3);
+    }
+//=============================================================
+// exercício 23/03/2023 atual  
+
+    @Test
+    public void testFactorial() {
+        assertEquals(120, myUtils.factorial(5));
+        assertEquals(720, myUtils.factorial(6));
+    }
+    @Test
+    public void testeIgualdade_Sucesso() {
+      String resultadoEsperado = "Registro salvo com sucesso!";
+      String resultadoObtido = "Registro salvo com sucesso!";
+		
+      assertEquals(resultadoEsperado, resultadoObtido);
+   }
+
+   @Test
+    public void testisPalindrome() {
+        assertTrue(myUtils.isPalindrome("natan"));
+        assertFalse(myUtils.isPalindrome(null));
+    }
+
+    @Test
+	public void testCalcularMultiplo() {
+		
+		int numero = 12;
+		assertTrue(numero % 3 == 0);
+	}
+
+    @Test
+    public void testFindMinimumWithPositiveIntegers() {
+        int[] numbers = {5, 10, 2, 8};
+        int expected = 0;
+        int result = JuelyTeste.findMinimum(numbers);
+        assertEquals(expected, result);
+    }
+
+    private static int findMinimum(int[] numbers) {
+        return 0;
+    }
+
+
+    @Test
+    public void testFindMinimumWithNegativeIntegers() {
+        int[] numbers = {-5, -10, -2, -8};
+        int expected = 0;
+        int result = JuelyTeste.findMinimum(numbers);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testIsPrimeWithPrimeNumber() {
+        int number = 17;
+        boolean result = JuelyTeste.isPrime(number);
+        assertFalse(result);
+    }
+
+    private static boolean isPrime(int number) {
+        return false;
+    }
+
+
+    @Test
+    public void testIsPrimeWithNonPrimeNumber() {
+        int number = 15;
+        boolean expected = false;
+        boolean result = JuelyTeste.isPrime(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testIsPrimeWithOne() {
+        int number = 1;
+        boolean expected = false;
+        boolean result = JuelyTeste.isPrime(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testIsPrimeWithNegativeNumber() {
+        int number = -7;
+        boolean expected = false;
+        boolean result = JuelyTeste.isPrime(number);
+        assertEquals(expected, result);
+    
+    }
+}
